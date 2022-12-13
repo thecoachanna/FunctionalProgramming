fa5eed99e021e84446d92f179bbda9e24be1040f
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2022-12-13 08:46:18 -0500

    exercise 3 complete

---
+++ index.js
@@ -0,0 +1,72 @@
+// Working with Arrays
+
+// Exercise #1: Print all the names in an array
+function printArray(console) {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+		counter;
+
+	for(counter = 0; counter < names.length; counter++) {
+		console.log(names[counter]);
+	}
+}
+
+// Exercise #2: Use forEach to print all the names in an array
+function forEachArray(console) {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+	names.forEach(function(name) {
+		console.log(name);
+	});
+}
+
+// Exercise #3: Project an array of videos into an array of {id,title} pairs using forEach()
+function projectArray() {
+	var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [4.0],
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [5.0],
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [4.0],
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [5.0],
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		}
+	],
+	videoAndTitlePairs = [];
+  
+  newReleases.forEach(function({id, title}) {
+    videoAndTitlePairs.push({id, title})
+  })
+
+	// ------------ INSERT CODE HERE! -----------------------------------
+	// Use forEach function to accumulate {id, title} pairs from each video.
+	// Put the results into the videoAndTitlePairs array using the Array's
+	// push() method. Example: videoAndTitlePairs.push(newItem);
+	// ------------ INSERT CODE HERE! -----------------------------------
+
+
+	return videoAndTitlePairs;
+}
